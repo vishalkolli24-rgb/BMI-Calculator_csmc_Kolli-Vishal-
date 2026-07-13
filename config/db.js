5f7e96fc34ee6274e0cb2ed845ac9f63ca56e435
Vishalchowdarykolli <vishal.kolli24@sasi.ac.in>
2026-07-13 15:02:53 +0530

    Refactor db.js to maintain connection logic

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const dns = require("dns")
 dns.setServer(["8.8.8.8",8.8.4.4"])
+ 
 const dbURI = 'mongodb+srv://vishalkolli21:vishalchowdarykolli21@cluster0.8zoym81.mongodb.net/vishalchowdarykolli'; 
 
 const connectDB = () => {
