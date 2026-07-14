290e0f27dfd70e88e4053f19aed9fd2fbbccc942
Vishalchowdarykolli <vishal.kolli24@sasi.ac.in>
2026-07-14 11:00:37 +0530

    Add age field to Bmi schema

--- models/Bmi.js
+++ models/Bmi.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const bmiSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  age :{ type: Number, required: true },
   height: { type: Number, required: true },
   weight: { type: Number, required: true },
   bmi: { type: Number, required: true },
